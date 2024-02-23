1fe9aaf95a32f76e0a9684fc6e1122ae4880e3d0
miquelpomar <miguelpomar@alumnat.iesdamiahuguet.net>
2024-02-23 08:19:46 +0100

    tria un color aleatori

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+let colors = ["#fbc204","#0080ff","#aafbd7","#e2cffd","#ffbf80","#f68a8a","#72e9ff","#77fd0d","#c9ef9a","#8d9702","#4fdd6e","#b83a04" ]
+let colorsjugats=[]
+
+
+/*
+Iniciar partida. Es posa 'async' davant per indicar que té
+comandes que gestionen el temps d'execució. Pot haver "esperes"
+*/
+async function iniciarPartida()
+{
+    colorsjugats.push(colors[Math.trunc(Math.random()*colors.length)])
+    for (let i=0;i<colorsjugats.length;i++){
+        showNotification(colorsjugats[i])
+        //Crida al mètode esperar
+        await esperar(1000);
+    }
+}
+function esperar(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+
+
 /*
     Mostra la notificació al panell notification
     Mostra el color hexadecimal passat per paràmetre
